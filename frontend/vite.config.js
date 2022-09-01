5c2cfe9c1cbd9e61b2a5f7ad560fadaaa040332d
Balakrishna <sanibalakrishna@gmail.com>
2022-09-01 12:45:20 +0530

    my commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000/",
+    },
+    "/api/user": {
+      target: "http://localhost:5000/",
+      pathRewrite: { "^/api/user": "/api" },
+    },
+  },
+  plugins: [react()],
+});
